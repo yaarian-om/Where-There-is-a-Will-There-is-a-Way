876cc231b58ee2c90c62da6c71af388e31ef926b
yaarian-om <dip.kumar020@gmail.com>
2022-08-02 09:18:47 +0600

    Updating 1st Screen Display

--- Where_There_is_a_Will_There_is_a_Way/main.cpp
+++ Where_There_is_a_Will_There_is_a_Way/main.cpp
@@ -16,10 +16,162 @@ void Display1();
 
 
 
-#ifndef Position vertex and Speed
+
+//#ifndef Position vertex and Speed
 GLfloat position = -1.2f,position2 = 0.0f,skypos=-1.2f,xpos=-1.0f,ypos=0.45f,yuppos,ydown;
 GLfloat speed = 0.08f,skyspeed=0.03f,crowspeed = 0.1f;
-#endif Position vertex and Speed
+//#endif Position vertex and Speed
+
+//#ifndef Extra Variables
+int donef = 0,dtwof = 0,dthreef = 0,dfourf = 0,dfivef = 0,dsixf = 0,dsevenf = 0,
+waterflag = 0,ideaflag = 0,ideamusic = 0,febbleflag = 0,stonereturnflag = 0,
+deightf = 0,flyaway = 0,drankwater = 0,happilygone = 0;
+//#endif Extra Variables
+
+
+
+
+void StartingText()
+{
+    char text[120];
+    sprintf(text, "WHERE THERE IS A WILL  THERE'S A WAY",5.00,8.00);
+    glColor3f(0, 0, 0);
+    glRasterPos2f( -28 , 32 );
+    for(int i = 0; text[i] != '\0'; i++)
+    {
+        if(text[i]==' '  && text[i+1]==' ')
+        {
+            glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24, text[i]);
+            glRasterPos2f( -20 , 22 );
+        }
+        else glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24, text[i]);
+    }
+
+    sprintf(text,"IF YOU ARE DETERMINED ENOUGH,YOU CAN FIND A WAY TO ACHEIEVE WHAT YOU WANT  EVEN IF IT IS VERY DIFFICULT",5.00,8.00);
+    glColor3f(0, 0, 0);
+    glRasterPos2f( -100 , 0 );
+    for(int i = 0; text[i] != '\0'; i++)
+    {
+          if(text[i]==' '  && text[i+1]==' ')
+        {
+            glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24, text[i]);
+            glRasterPos2f( -40 , -10 );
+        }
+        else glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24, text[i]);
+    }
+    sprintf(text,"Press 1,2,3,4,5,6,7 to continue the story",5.00,8.00);
+    glColor3f(0, 0, 0);
+    glRasterPos2f( -42 , -30 );
+    for(int i = 0; text[i] != '\0'; i++)
+    {
+          if(text[i]==' '  && text[i+1]==' ')
+        {
+            glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24, text[i]);
+            glRasterPos2f( -40 , -10 );
+        }
+        else glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24, text[i]);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+void Display1()
+{
+    glClearColor(1.0f, 1.0f, 1.0f, 1.0f); /// Set background color to black and opaque
+    glClear(GL_COLOR_BUFFER_BIT);
+    glBegin(GL_POLYGON);
+    glColor3ub(124, 252, 0);
+    glVertex2f(-1.0f,-0.85f);
+    glVertex2f(1.0f,-0.7f);
+    glVertex2f(1.0f,-1.0);
+    glVertex2f(-1.0f,-1.0);
+    glEnd();
+
+    //fullsky();
+    //grass();
+    //background();
+    //sky1();
+    //sky2();
+
+    glLoadIdentity();
+    glTranslatef(0.0,0.0,0.0);
+    //Normal_Tree();
+    glLoadIdentity();
+    glTranslatef(0.6,0.0,0.0);
+   // Normal_Tree();
+    glLoadIdentity();
+    glTranslatef(1.2,0.0,0.0);
+    //Normal_Tree();
+    glLoadIdentity();
+    glTranslatef(-0.17,0.0,0.0);
+   // sun();
+    glLoadIdentity();
+    glFlush();
+
+}
+
+void handleKeypress(unsigned char key, int x, int y) {
+	switch (key) {
+	    case '1':
+	        std::cout<<"1 Pressed"<<std::endl;
+            glutDestroyWindow(1);
+            glutInitWindowSize(1240, 680);
+            glutInitWindowPosition((glutGet(GLUT_SCREEN_WIDTH)-1240)/2,(glutGet(GLUT_SCREEN_HEIGHT)-680)/2);
+            glutCreateWindow("MORAL STORY");
+            glutKeyboardFunc(handleKeypress);
+            glutDisplayFunc(Display1);
+            break;
+        case '2':
+            std::cout<<"2 Pressed"<<std::endl;
+            position=-1.0f;
+            //glutDisplayFunc(Display2);
+            break;
+        case '3':
+            std::cout<<"3 Pressed"<<std::endl;
+            position=-1.0f;
+            //glutDisplayFunc(Display3);
+            break;
+        case '4':
+            std::cout<<"4 Pressed"<<std::endl;
+            position=-1.0f;
+            //glutDisplayFunc(Display4);
+            break;
+        case '5':
+            std::cout<<"5 Pressed"<<std::endl;
+            position=-0.9f;
+            //glutDisplayFunc(Display5);
+            break;
+        case '6':
+            std::cout<<"6 Pressed"<<std::endl;
+            position=-0.9f;
+            xpos = -1.0 ;
+            ypos = 0.45;
+            //glutDisplayFunc(Display6);
+            break;
+        case '7':
+            std::cout<<"7 Pressed"<<std::endl;
+            position=-0.9f;
+            xpos = -1.4 ;
+            ypos = -0.85;
+            yuppos=ypos;
+            febbleflag = 1;
+            stonereturnflag = 1;
+            //glutDisplayFunc(Display7);
+            break;
+    }
+}
 
 
 
@@ -89,6 +241,7 @@ void Display(void)
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glLoadIdentity(); ///
     glTranslatef(0,0,-20);
+    StartingText(); /// That will bring up the starting text
     glFlush();
     glutSwapBuffers();
 }
@@ -113,11 +266,14 @@ void init(void)
 int main(int argc, char** argv)
 {
     glutInit(&argc,argv);
-    glutInitWindowSize(1920, 1080); /// Window size is similar to my screen resolution (1920 X 1080) pixel
-    glutInitWindowPosition(-10,0); /// Top Left corner of the screen position
+    glutInitWindowSize(1240, 680); /// Window size is similar to my screen resolution (1920 X 1080) pixel
+    glutInitWindowPosition((glutGet(GLUT_SCREEN_WIDTH)-1240)/2,(glutGet(GLUT_SCREEN_HEIGHT)-680)/2); /// Top Left corner of the screen position
     glutCreateWindow("MORAL STORY"); /// Screen Title
     init();
-    glutDisplayFunc(Display); /// calling Display function to show some display ==>> White Page
+    glutReshapeFunc(reshape);
+    glutDisplayFunc(Display); /// Calling Display function to show some display ==>> White Page
+    glutKeyboardFunc(handleKeypress);
+    glutTimerFunc(100, update, 0);
     glutMainLoop();
     return 0;
 }
